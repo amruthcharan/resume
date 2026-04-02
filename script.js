861760d3c5c0d1ca56382bdceb560cd132fdf890
Amruth Charan <amruthcharank@gmail.com>
2026-04-02 07:10:24 +0530

    FIxed typos

--- script.js
+++
@@ -1,9 +0,0 @@
-const changeTheme = type => {
-  document.documentElement.style.setProperty('--primary', `var(--${type})`);
-  document.documentElement.style.setProperty('--primary-light', `var(--${type}-transparent)`);
-};
-
-// for dark theme
-document.documentElement.classList.add('theme--dark');
-
-changeTheme('teal');
